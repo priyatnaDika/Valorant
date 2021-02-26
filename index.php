ae42cea66ec374e599c8b8067031cf564ceb8527
priyatnaDika <ngotaxdong008@gmail.com>
2021-02-26 19:51:57 +0800

    project saya

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Latihan 1</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="robots" content="index, folow, max-snippet:-1, max-image-preview:large, max-video-preview:-1">
+    <link rel="canonical" href="http://informatika.undiksha.ac.id/">
+    <meta property="og:locale" content="en_US">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="??">
+    <meta property="og:description" content="??">
+    <meta property="og:url" content="??">
+    <meta property="og:site_name" content="??">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+    <div class="jumbotron text-center", style="background-image: url(assets/projekAkhir.gif);  margin:235px; color: tomato;  height: 450px; padding: 250px;" >
+        
+        <h1> <b>GameBoy</b> </h1>
+        
+        <p>Eat|Sleep|Game|Repeat</p>
+    </div>
+
+    <div class="container" style="float: left; ">
+        <ul class="nav nav-tabs">
+            <li class="activate"><a href="#">Home</a></li>
+            <li><a href="#">Artikel</a></li>
+            <li><a href="#">ESPORTS</a></li>
+            <li><a href="#">Forum</a></li>
+        </ul>
+    </div>
+
+    <div class="post-info" style="float: left;">
+        Upload by <b>ADMIN</b>
+    </div>
+
+    <p></p>
+    <p></p>
+    <p></p>
+
+    <div class="container" style="padding: 200px;">
+        <div class="row">
+            <div >
+                <h3>GAME SYSTEM</h3>
+                <img src="media/IBLdfSU.gif", class="rounded float-left">
+                <p></p>
+                <p>Valorant adalah 5 versus 5 character-based tactical shooter. Setiap pertandingan akan berisi 25 round dan tim pertama yang berhasil meraih 13 round kemenangan akan memenangkan pertandingan. Anda dapat memilih Agent di awal pertandingan. Berbeda dengan Overwatch, Anda tidak bisa mengganti Agent di tengah pertandingan. Mode game-nya identik dengan CS:GO yaitu berupa bomb mission. Karena game FPS membutuhkan tingkat presisi yang tinggi, Riot Games akan menyediakan 128 tick-servers untuk Valorant.</p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+            </div>
+
+
+
+            <div >
+                <h3>Karakter dengan skill hasil gabungan dari Overwatch, Apex Legends dan Rainbow Six Siege</h3>
+                <img src="media/13-2.gif", class="img-rounded" width="800px", height="400px">
+                <p></p>
+                <p>Dikutip dari ESTNN, ada delapan karakter yang diperkenalkan pertama kali di Valorant. Setiap Agent memiliki skill yang berbeda satu sama lain. Skill setiap karakter dikategorikan menjadi tiga yaitu purchasable abilities, ultimate, dan main ability. Purchasable abilities adalah skill yang bisa dibeli saat memulai ronde. Anda dapat membeli dua jenis skill yang tersedia. Ultimate skill didapat dari membunuh musuh dan menyelesaikan objektif. Mekanik ultimate yang sama dengan Overwatch. Main ability adalah skill yang tersedia di setiap karakter secara cuma-cuma. Main ability membutuhkan cooldown pada setiap penggunaannya.</p>
+                <p>Tidak seperti Overwatch yang berfokus pada skill, di Valorant ini kamu masih tetap mengutamakan penggunaan senjata untuk berperang. Bisa dibilang bahkan skill dari tiap karakter yang ada bukanlah penentu kemenangan karena tiap skill memiliki kegunaannya masing-masing, serta skill tersebut masih bisa diatasi dengan mudah.</p>
+                <p></p>
+                <p></p>
+                <p></p>
+            </div>
+
+
+
+
+            <div >
+                <h3>System Requirements</h3>
+                <img src="media/tenor.gif", class="rounded float-left">
+                <p></p>
+                <p>Buat kalian yang memiliki laptop dengan spesifikasi yang terbilang pas-pasan tapi ingin bermain Valorant sebaiknya kalian tidak usah khawatir karena Valorant membutuhkan spesifikasi yang tidak begitu tinggi untuk dijalankan dengan lancar. Jika kamu ingin memainkan Valorant dengan frame per second rata-rata sebesar 30 FPS, maka setidaknya laptop atau PC kamu menggunakan prosesor Intel i3-370M dengan VGA Intel HD 4000. Dengan spesifikasi tersebut kamu bisa memainkan Valorant di 30 FPS dengan pengaturan grafis rata kiri. Sedangkan untuk menjalankan di 60 FPS, kamu hanya perlu menggunakan prosesor Intel i3-4150 dengan VGA Geforce GT 730.</p>
+                <p>Kesimpulannya, Valorant ini sangatlah cocok untuk dimainkan oleh pecinta game FPS, khususnya FPS berjenis tactical shooter. Jika kamu mulai merasa bosan dengan game FPS yang sedang kamu mainkan, maka tidak ada salahnya kamu mencoba Valorant untuk merasakan gameplay yang baru dan fresh!</p>
+                <p></p>
+                <p></p>
+                <p></p>
+            </div>
+        </div>
+    </div>
+
+</body>
